5cb74d2151d1a372a4e58c7603d238029016e5e3
Donne Martin <donne.martin@gmail.com>
2016-01-09 15:43:46 -0500

    Site: Use relative urls.

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -32,4 +32,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
