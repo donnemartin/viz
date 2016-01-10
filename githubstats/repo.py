8e9cce88161c56cebfc78361aea4d02ab6fc0575
Donne Martin <donne.martin@gmail.com>
2016-01-10 07:10:52 -0500

    Fix linter issues.

--- githubstats/repo.py
+++ githubstats/repo.py
@@ -13,6 +13,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+
 class Repo(object):
     """GitHub repo.
 
