20b36aaa8d195dda5c5b6b5235ce096962dc0f85
Donne Martin <donne.martin@gmail.com>
2016-01-10 08:50:23 -0500

    Add test expected csv output.

---
+++ tests/data/expected_csv.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+expected_csv = ['full_name stars forks description language', 'Google/foo 250 0 "foo" Python', 'Google/bar 650 0 "bar" JavaScript', 'Facebook/baz 270 0 "baz" Python', 'Facebook/qux 550 0 "qux" JavaScript', 'Facebook/foobar 70 0 "foobar" ', 'donnemartin/foobaz 50 0 "foobaz" Python', 'donnemartin/barfoo 100 0 "barfoo" Python', 'donnemartin/barbaz 150 0 "barbaz" ']
